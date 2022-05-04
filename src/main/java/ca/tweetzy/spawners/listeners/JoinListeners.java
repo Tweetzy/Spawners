a75e5bb14248fba43d8aa59f9e8577f9ea38f22a
Kiran Hart <kiran.hart@live.com>
2022-05-04 18:01:08 -0400

    temporary hex replacemetn for chat

--- src/main/java/ca/tweetzy/spawners/listeners/JoinListeners.java
+++ src/main/java/ca/tweetzy/spawners/listeners/JoinListeners.java
@@ -1,9 +1,11 @@
 package ca.tweetzy.spawners.listeners;
 
+import ca.tweetzy.rose.utils.Common;
 import ca.tweetzy.spawners.Spawners;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.player.AsyncPlayerChatEvent;
 import org.bukkit.event.player.PlayerJoinEvent;
 
 /**
@@ -20,5 +22,10 @@ public final class JoinListeners implements Listener {
 
 		if (Spawners.getPlayerManager().findUser(player) == null)
 			Spawners.getPlayerManager().createPlayer(player, null);
+	}
+
+	@EventHandler
+	public void onChat(AsyncPlayerChatEvent chatEvent) {
+		chatEvent.setMessage(Common.colorize(chatEvent.getMessage()));
 	}
 }
