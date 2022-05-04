fa3c87807b4cac8ab8b4f542703fac5eeee45e3c
Kiran Hart <kiran.hart@live.com>
2022-05-04 18:01:28 -0400

    create main admin menu with level button

---
+++ src/main/java/ca/tweetzy/spawners/guis/SpawnersAdminGUI.java
@@ -0,0 +1,32 @@
+package ca.tweetzy.spawners.guis;
+
+import ca.tweetzy.rose.comp.enums.CompMaterial;
+import ca.tweetzy.rose.gui.template.BaseGUI;
+import ca.tweetzy.rose.utils.QuickItem;
+import ca.tweetzy.spawners.Spawners;
+import ca.tweetzy.spawners.guis.levels.LevelListAdminGUI;
+import ca.tweetzy.spawners.settings.Translation;
+
+/**
+ * Date Created: May 04 2022
+ * Time Created: 5:16 p.m.
+ *
+ * @author Kiran Hart
+ */
+public final class SpawnersAdminGUI extends BaseGUI {
+
+	public SpawnersAdminGUI() {
+		super(null, Translation.GUI_ADMIN_MAIN_TITLE.getString("plugin_version", Spawners.getInstance().getVersion()), 6);
+		draw();
+	}
+
+	@Override
+	protected void draw() {
+		// level presets
+		setButton(1, 1, QuickItem
+				.of(CompMaterial.TRIPWIRE_HOOK)
+				.name(Translation.GUI_ADMIN_MAIN_ITEMS_LEVELS_NAME.getString())
+				.lore(Translation.GUI_ADMIN_MAIN_ITEMS_LEVELS_LORE.getList())
+				.make(), click -> click.manager.showGUI(click.player, new LevelListAdminGUI()));
+	}
+}
--- src/main/java/ca/tweetzy/spawners/settings/Translation.java
+++ src/main/java/ca/tweetzy/spawners/settings/Translation.java
@@ -4,6 +4,7 @@ import ca.tweetzy.rose.utils.Replacer;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
+import java.util.Arrays;
 import java.util.List;
 
 /**
@@ -27,6 +28,36 @@ public enum Translation {
 	NOT_A_NUMBER("misc.not a number", "&cThat is not a valid number"),
 	PLAYER_OFFLINE("misc.player not found", "&cThe player &4%player% &cis not online!"),
 
+	/*
+	============= guis =============
+	 */
+	GUI_ADMIN_MAIN_TITLE("gui.admin main.title", "<GRADIENT:fc67fa>&LSpawners</GRADIENT:f4c4f3> &7v%plugin_version%"),
+	GUI_ADMIN_MAIN_ITEMS_LEVELS_NAME("gui.admin main.items.level.name", "<GRADIENT:fc67fa>&lLevels</GRADIENT:f4c4f3>"),
+	GUI_ADMIN_MAIN_ITEMS_LEVELS_LORE("gui.admin main.items.level.lore", Arrays.asList(
+			"&7View all the known levels",
+			"",
+			"&e&lClick &8» &7To view levels"
+	)),
+
+	GUI_LEVEL_ADMIN_LIST_TITLE("gui.level admin list.title", "<GRADIENT:fc67fa>&LSpawners</GRADIENT:f4c4f3> &8> &7Levels"),
+	GUI_LEVEL_ADMIN_LIST_LEVELS_NAME("gui.level admin list.items.level.name", "<GRADIENT:fc67fa>&lLevel %level_number%</GRADIENT:f4c4f3>"),
+	GUI_LEVEL_ADMIN_LIST_LEVELS_LORE("gui.level admin list.items.level.lore", Arrays.asList(
+			"&8Spawner Level",
+			"",
+			"&7Spawn Interval&F: &e%level_spawn_interval%",
+			"&7Spawn Count&F: &e%level_spawn_count%",
+			"&7Max Nearby Entities&F: &e%level_max_nearby_entities%",
+			"&7Player Activation Range&F: &e%level_player_activation_range%",
+			"",
+			"",
+			"&e&lLeft Click &8» &7To edit level",
+			"&c&lPress 1 &8» &7To delete level"
+	)),
+
+	GUI_LEVEL_ADMIN_LIST_NEW_NAME("gui.level admin list.items.create.name", "&a&lCreate Level"),
+	GUI_LEVEL_ADMIN_LIST_NEW_LORE("gui.level admin list.items.create.lore", List.of(
+			"&e&lClick &8» &7To create new level"
+	)),
 
 	;
 
