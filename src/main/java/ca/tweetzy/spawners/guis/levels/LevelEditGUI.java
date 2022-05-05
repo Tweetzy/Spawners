26b2cf73746d34a8214a82cc694057e962629627
Kiran Hart <kiran.hart@live.com>
2022-05-04 22:22:42 -0400

    properly replace level # variable in title input

--- src/main/java/ca/tweetzy/spawners/guis/levels/LevelEditGUI.java
+++ src/main/java/ca/tweetzy/spawners/guis/levels/LevelEditGUI.java
@@ -19,7 +19,7 @@ public final class LevelEditGUI extends BaseGUI {
 	private final Level level;
 
 	public LevelEditGUI(@NonNull final Level level) {
-		super(new LevelListAdminGUI(), Translation.GUI_LEVEL_EDIT_TITLE.getString("level_number", level.getLevel()), 6);
+		super(new LevelListAdminGUI(), Translation.GUI_LEVEL_EDIT_TITLE.getString("level_number", level.getLevel()), 4);
 		this.level = level;
 		draw();
 	}
@@ -29,7 +29,7 @@ public final class LevelEditGUI extends BaseGUI {
 		setButton(1, 1, QuickItem.of(CompMaterial.REPEATER)
 				.name(Translation.GUI_LEVEL_EDIT_ITEMS_SPAWN_DELAY_NAME.getString())
 				.lore(Translation.GUI_LEVEL_EDIT_ITEMS_SPAWN_DELAY_LORE.getList("level_spawn_interval", this.level.getSpawnInterval()))
-				.make(), click -> UserInput.askInteger(click.player, Translation.INPUT_LEVEL_EDIT_TITLE.getString(), Translation.INPUT_LEVEL_EDIT_SPAWN_DELAY.getString(), value -> {
+				.make(), click -> UserInput.askInteger(click.player, Translation.INPUT_LEVEL_EDIT_TITLE.getString("level_number", this.level.getLevel()), Translation.INPUT_LEVEL_EDIT_SPAWN_DELAY.getString(), value -> {
 
 			this.level.setSpawnInterval(value);
 			click.manager.showGUI(click.player, new LevelEditGUI(this.level));
@@ -38,7 +38,7 @@ public final class LevelEditGUI extends BaseGUI {
 		setButton(1, 3, QuickItem.of(CompMaterial.REDSTONE_TORCH)
 				.name(Translation.GUI_LEVEL_EDIT_ITEMS_SPAWN_COUNT_NAME.getString())
 				.lore(Translation.GUI_LEVEL_EDIT_ITEMS_SPAWN_COUNT_LORE.getList("level_spawn_count", this.level.getSpawnCount()))
-				.make(), click -> UserInput.askInteger(click.player, Translation.INPUT_LEVEL_EDIT_TITLE.getString(), Translation.INPUT_LEVEL_EDIT_SPAWN_COUNT.getString(), value -> {
+				.make(), click -> UserInput.askInteger(click.player, Translation.INPUT_LEVEL_EDIT_TITLE.getString("level_number", this.level.getLevel()), Translation.INPUT_LEVEL_EDIT_SPAWN_COUNT.getString(), value -> {
 
 			this.level.setSpawnCount(value);
 			click.manager.showGUI(click.player, new LevelEditGUI(this.level));
@@ -47,7 +47,7 @@ public final class LevelEditGUI extends BaseGUI {
 		setButton(1, 5, QuickItem.of(CompMaterial.OBSERVER)
 				.name(Translation.GUI_LEVEL_EDIT_ITEMS_NEARBY_ENTITIES_NAME.getString())
 				.lore(Translation.GUI_LEVEL_EDIT_ITEMS_NEARBY_ENTITIES_LORE.getList("level_max_nearby_entities", this.level.getMaxNearbyEntities()))
-				.make(), click -> UserInput.askInteger(click.player, Translation.INPUT_LEVEL_EDIT_TITLE.getString(), Translation.INPUT_LEVEL_EDIT_MAX_NEARBY_ENTITIES.getString(), value -> {
+				.make(), click -> UserInput.askInteger(click.player, Translation.INPUT_LEVEL_EDIT_TITLE.getString("level_number", this.level.getLevel()), Translation.INPUT_LEVEL_EDIT_MAX_NEARBY_ENTITIES.getString(), value -> {
 
 			this.level.setMaxNearbyEntities(value);
 			click.manager.showGUI(click.player, new LevelEditGUI(this.level));
@@ -55,11 +55,13 @@ public final class LevelEditGUI extends BaseGUI {
 
 		setButton(1, 7, QuickItem.of(CompMaterial.COMPASS)
 				.name(Translation.GUI_LEVEL_EDIT_ITEMS_ACTIVATION_RANGE_NAME.getString())
-				.lore(Translation.GUI_LEVEL_EDIT_ITEMS_ACTIVATION_RANGE_LORE.getList("level_max_nearby_entities", this.level.getPlayerActivationRange()))
-				.make(), click -> UserInput.askInteger(click.player, Translation.INPUT_LEVEL_EDIT_TITLE.getString(), Translation.INPUT_LEVEL_EDIT_ACTIVATION_RANGE.getString(), value -> {
+				.lore(Translation.GUI_LEVEL_EDIT_ITEMS_ACTIVATION_RANGE_LORE.getList("level_player_activation_range", this.level.getPlayerActivationRange()))
+				.make(), click -> UserInput.askInteger(click.player, Translation.INPUT_LEVEL_EDIT_TITLE.getString("level_number", this.level.getLevel()), Translation.INPUT_LEVEL_EDIT_ACTIVATION_RANGE.getString(), value -> {
 
 			this.level.setPlayerActivationRange(value);
 			click.manager.showGUI(click.player, new LevelEditGUI(this.level));
 		}));
+
+		applyBackExit();
 	}
 }
