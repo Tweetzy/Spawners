ea24b41a39de2862b2ba831fc314c51e30dc5dba
Kiran Hart <kiran.hart@live.com>
2022-05-04 21:48:05 -0400

    level list admin gui can create levels

--- src/main/java/ca/tweetzy/spawners/guis/levels/LevelListAdminGUI.java
+++ src/main/java/ca/tweetzy/spawners/guis/levels/LevelListAdminGUI.java
@@ -8,6 +8,7 @@ import ca.tweetzy.rose.utils.QuickItem;
 import ca.tweetzy.spawners.Spawners;
 import ca.tweetzy.spawners.api.interfaces.Level;
 import ca.tweetzy.spawners.guis.SpawnersAdminGUI;
+import ca.tweetzy.spawners.impl.SpawnerLevel;
 import ca.tweetzy.spawners.settings.Translation;
 import org.bukkit.inventory.ItemStack;
 
@@ -30,7 +31,7 @@ public final class LevelListAdminGUI extends PagedGUI<Level> {
 	protected ItemStack makeDisplayItem(Level level) {
 		return QuickItem.of(CompMaterial.PAPER)
 				.name(Translation.GUI_LEVEL_ADMIN_LIST_LEVELS_NAME.getString("level_number", level.getLevel()))
-				.lore(Translation.GUI_LEVEL_ADMIN_LIST_LEVELS_LORE.getString(
+				.lore(Translation.GUI_LEVEL_ADMIN_LIST_LEVELS_LORE.getList(
 						"level_spawn_interval", level.getSpawnInterval(),
 						"level_spawn_count", level.getSpawnCount(),
 						"level_max_nearby_entities", level.getMaxNearbyEntities(),
@@ -44,9 +45,13 @@ public final class LevelListAdminGUI extends PagedGUI<Level> {
 		setButton(5, 4, QuickItem.of(CompMaterial.SLIME_BALL)
 				.name(Translation.GUI_LEVEL_ADMIN_LIST_NEW_NAME.getString())
 				.lore(Translation.GUI_LEVEL_ADMIN_LIST_NEW_LORE.getList())
-				.make(), click -> {
+				.make(), click -> Spawners.getDataManager().insertLevel(new SpawnerLevel(Spawners.getLevelManager().getHighestLevel() + 1), (error, createdLevel) -> {
 
-		});
+			if (error != null) return;
+			Spawners.getLevelManager().addLevel(createdLevel);
+
+			click.manager.showGUI(click.player, new LevelListAdminGUI());
+		}));
 	}
 
 	@Override
