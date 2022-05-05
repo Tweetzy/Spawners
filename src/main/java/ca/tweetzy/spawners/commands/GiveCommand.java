597e5b5e0e062998f80b51cd334a474ae0f3f227
Kiran Hart <kiran.hart@live.com>
2022-05-04 23:09:59 -0400

    temp give command

---
+++ src/main/java/ca/tweetzy/spawners/commands/GiveCommand.java
@@ -0,0 +1,53 @@
+package ca.tweetzy.spawners.commands;
+
+import ca.tweetzy.rose.command.AllowedExecutor;
+import ca.tweetzy.rose.command.Command;
+import ca.tweetzy.rose.command.ReturnType;
+import ca.tweetzy.spawners.model.SpawnerItem;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.EntityType;
+import org.bukkit.entity.Player;
+
+import java.util.List;
+
+/**
+ * Date Created: May 04 2022
+ * Time Created: 10:43 p.m.
+ *
+ * @author Kiran Hart
+ */
+public final class GiveCommand extends Command {
+
+	public GiveCommand() {
+		super(AllowedExecutor.PLAYER, "give");
+	}
+
+	@Override
+	protected ReturnType execute(CommandSender sender, String... args) {
+		final Player player = (Player) sender;
+
+		player.getInventory().addItem(SpawnerItem.make(player, EntityType.COW, null));
+
+		return ReturnType.SUCCESS;
+	}
+
+	@Override
+	protected List<String> tab(CommandSender sender, String... args) {
+		return null;
+	}
+
+	@Override
+	public String getPermissionNode() {
+		return null;
+	}
+
+	@Override
+	public String getSyntax() {
+		return null;
+	}
+
+	@Override
+	public String getDescription() {
+		return null;
+	}
+}
