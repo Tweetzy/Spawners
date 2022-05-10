b3928dda4093c475866a9b043d1c08d839034c7e
Kiran Hart <kiran.hart@live.com>
2022-05-10 13:10:15 -0400

    create and implement Preset -> SpawnerPreset
    Took 5 minutes

---
+++ src/main/java/ca/tweetzy/spawners/api/spawner/Preset.java
@@ -0,0 +1,14 @@
+package ca.tweetzy.spawners.api.spawner;
+
+/**
+ * Date Created: May 10 2022
+ * Time Created: 1:03 p.m.
+ *
+ * @author Kiran Hart
+ */
+public interface Preset {
+
+	String getId();
+
+	Spawner getSpawner();
+}
---
+++ src/main/java/ca/tweetzy/spawners/impl/SpawnerPreset.java
@@ -0,0 +1,28 @@
+package ca.tweetzy.spawners.impl;
+
+import ca.tweetzy.spawners.api.spawner.Preset;
+import ca.tweetzy.spawners.api.spawner.Spawner;
+import lombok.AllArgsConstructor;
+
+/**
+ * Date Created: May 10 2022
+ * Time Created: 1:04 p.m.
+ *
+ * @author Kiran Hart
+ */
+@AllArgsConstructor
+public final class SpawnerPreset implements Preset {
+
+	private final String id;
+	private final Spawner spawner;
+
+	@Override
+	public String getId() {
+		return this.id;
+	}
+
+	@Override
+	public Spawner getSpawner() {
+		return this.spawner;
+	}
+}
