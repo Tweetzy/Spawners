9d2778ab5311c8f1f87f934bd73443eeeb39a174
Kiran Hart <kiran.hart@live.com>
2022-05-13 11:48:42 -0400

    create spawner inteval level
    Took 6 seconds

---
+++ src/main/java/ca/tweetzy/spawners/impl/level/SpawnIntervalLevel.java
@@ -0,0 +1,27 @@
+package ca.tweetzy.spawners.impl.level;
+
+import ca.tweetzy.spawners.api.LevelOption;
+import ca.tweetzy.spawners.api.spawner.Level;
+
+/**
+ * Date Created: May 13 2022
+ * Time Created: 11:10 a.m.
+ *
+ * @author Kiran Hart
+ */
+public final class SpawnIntervalLevel extends Level {
+
+	public SpawnIntervalLevel(int level, double cost, final int spawnInterval) {
+		super(LevelOption.SPAWN_INTERVAL, level, spawnInterval, cost);
+	}
+
+	@Override
+	public void setValue(int value) {
+		this.value = value;
+	}
+
+	@Override
+	public void setCost(double cost) {
+		this.cost = cost;
+	}
+}
