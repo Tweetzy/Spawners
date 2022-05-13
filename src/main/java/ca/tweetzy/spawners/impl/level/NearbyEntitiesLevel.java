abdc86b7803f146f93fc4c2c278411f4aaf605bd
Kiran Hart <kiran.hart@live.com>
2022-05-13 11:48:24 -0400

    create nearby entities level
    Took 9 seconds

---
+++ src/main/java/ca/tweetzy/spawners/impl/level/NearbyEntitiesLevel.java
@@ -0,0 +1,27 @@
+package ca.tweetzy.spawners.impl.level;
+
+import ca.tweetzy.spawners.api.LevelOption;
+import ca.tweetzy.spawners.api.spawner.Level;
+
+/**
+ * Date Created: May 13 2022
+ * Time Created: 11:15 a.m.
+ *
+ * @author Kiran Hart
+ */
+public final class NearbyEntitiesLevel extends Level{
+
+	public NearbyEntitiesLevel(int level, double cost, int maxNearbyEntities) {
+		super(LevelOption.MAX_NEARBY_ENTITIES, level, maxNearbyEntities, cost);
+	}
+
+	@Override
+	public void setValue(int value) {
+		this.value = value;
+	}
+
+	@Override
+	public void setCost(double cost) {
+		this.cost = cost;
+	}
+}
