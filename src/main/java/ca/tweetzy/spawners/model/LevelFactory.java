4ffa2a24297598a2c01baada3c0dd7df3cb78bbf
Kiran Hart <kiran.hart@live.com>
2022-05-13 15:17:51 -0400

    added getDefaultValue(LevelOption) to LevelFactory
    Took 16 seconds

--- src/main/java/ca/tweetzy/spawners/model/LevelFactory.java
+++ src/main/java/ca/tweetzy/spawners/model/LevelFactory.java
@@ -6,6 +6,7 @@ import ca.tweetzy.spawners.impl.level.ActivationRangeLevel;
 import ca.tweetzy.spawners.impl.level.NearbyEntitiesLevel;
 import ca.tweetzy.spawners.impl.level.SpawnCountLevel;
 import ca.tweetzy.spawners.impl.level.SpawnIntervalLevel;
+import ca.tweetzy.spawners.settings.Settings;
 import lombok.experimental.UtilityClass;
 
 /**
@@ -23,6 +24,15 @@ public final class LevelFactory {
 			case SPAWN_COUNT -> new SpawnCountLevel(level, cost, value);
 			case MAX_NEARBY_ENTITIES -> new NearbyEntitiesLevel(level, cost, value);
 			case ACTIVATION_RANGE -> new ActivationRangeLevel(level, cost, value);
+		};
+	}
+
+	public int getDefaultValue(LevelOption levelOption) {
+		return switch (levelOption) {
+			case SPAWN_INTERVAL -> Settings.DEFAULT_SPAWNER_DELAY.getInt();
+			case SPAWN_COUNT -> Settings.DEFAULT_SPAWNER_SPAWN_COUNT.getInt();
+			case MAX_NEARBY_ENTITIES -> Settings.DEFAULT_SPAWNER_MAX_NEARBY_ENTITIES.getInt();
+			case ACTIVATION_RANGE -> Settings.DEFAULT_SPAWNER_ACTIVATION_RANGE.getInt();
 		};
 	}
 }
