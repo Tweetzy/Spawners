4a4615b0ab3dbad0bf202e3e37aad9f474e906ad
Kiran Hart <kiran.hart@live.com>
2022-05-13 11:48:33 -0400

    create spawner count level
    Took 7 seconds

---
+++ src/main/java/ca/tweetzy/spawners/impl/level/SpawnCountLevel.java
@@ -0,0 +1,27 @@
+package ca.tweetzy.spawners.impl.level;
+
+import ca.tweetzy.spawners.api.LevelOption;
+import ca.tweetzy.spawners.api.spawner.Level;
+
+/**
+ * Date Created: May 13 2022
+ * Time Created: 11:13 a.m.
+ *
+ * @author Kiran Hart
+ */
+public final class SpawnCountLevel extends Level {
+
+	public SpawnCountLevel(int level, double cost, final int spawnCount) {
+		super(LevelOption.SPAWN_COUNT, level, spawnCount,cost);
+	}
+
+	@Override
+	public void setValue(int value) {
+		this.value = value;
+	}
+
+	@Override
+	public void setCost(double cost) {
+		this.cost = cost;
+	}
+}
