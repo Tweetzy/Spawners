8d057c13831e3bbe7db775a12cef1c531793af66
Kiran Hart <kiran.hart@live.com>
2022-05-14 14:13:57 -0400

    capitalize level list title
    Took 36 seconds

--- src/main/java/ca/tweetzy/spawners/guis/admin/levels/LevelListGUI.java
+++ src/main/java/ca/tweetzy/spawners/guis/admin/levels/LevelListGUI.java
@@ -4,6 +4,7 @@ import ca.tweetzy.rose.comp.enums.CompMaterial;
 import ca.tweetzy.rose.gui.events.GuiClickEvent;
 import ca.tweetzy.rose.gui.helper.InventoryBorder;
 import ca.tweetzy.rose.gui.template.PagedGUI;
+import ca.tweetzy.rose.utils.ChatUtil;
 import ca.tweetzy.rose.utils.QuickItem;
 import ca.tweetzy.rose.utils.Replacer;
 import ca.tweetzy.spawners.Spawners;
@@ -27,7 +28,7 @@ public final class LevelListGUI extends PagedGUI<Level> {
 	private final LevelOption levelOption;
 
 	public LevelListGUI(@NonNull final LevelOption levelOption) {
-		super(new LevelOptionSelectGUI(), "<GRADIENT:fc67fa>&LLevels</GRADIENT:f4c4f3> &8> " + levelOption.name(), 6, Spawners.getLevelManager().getLevels(levelOption));
+		super(new LevelOptionSelectGUI(), "<GRADIENT:fc67fa>&LLevels</GRADIENT:f4c4f3> &8> " + ChatUtil.capitalizeFully(levelOption.name()), 6, Spawners.getLevelManager().getLevels(levelOption));
 		this.levelOption = levelOption;
 		draw();
 	}
