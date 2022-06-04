9d8e3a00493ce559595a0ded27812720eacccfb4
Kiran Hart <kiran.hart@live.com>
2022-06-04 00:17:25 -0400

    implement api

--- src/main/java/ca/tweetzy/spawners/api/SpawnersAPI.java
+++ src/main/java/ca/tweetzy/spawners/api/SpawnersAPI.java
@@ -17,6 +17,23 @@
  */
 package ca.tweetzy.spawners.api;
 
+import ca.tweetzy.spawners.api.spawner.Level;
+import ca.tweetzy.spawners.api.spawner.Preset;
+import ca.tweetzy.spawners.api.spawner.Spawner;
+import ca.tweetzy.spawners.api.spawner.SpawnerUser;
+import lombok.NonNull;
+import org.bukkit.Chunk;
+import org.bukkit.Location;
+import org.bukkit.block.CreatureSpawner;
+import org.bukkit.entity.EntityType;
+import org.bukkit.entity.Player;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.List;
+import java.util.UUID;
+import java.util.function.BiConsumer;
+import java.util.function.Consumer;
+
 /**
  * Date Created: May 04 2022
  * Time Created: 10:44 a.m.
@@ -24,4 +41,67 @@ package ca.tweetzy.spawners.api;
  * @author Kiran Hart
  */
 public interface SpawnersAPI {
+
+	void addLevel(@NonNull final Level level);
+
+	void removeLevel(@NonNull final Level level);
+
+	void createLevel(@NonNull final Level level, final BiConsumer<Boolean, Level> consumer);
+
+	void deleteLevel(@NonNull final Level level, final Consumer<Boolean> consumer);
+
+	Level findLevel(@NonNull final LevelOption levelOption, final int levelNumber);
+
+	List<Level> getLevels();
+
+	List<Level> getLevels(@NonNull final LevelOption levelOption);
+
+	int getHighestLevel(@NonNull final LevelOption levelOption);
+
+	void addSpawner(@NotNull Spawner spawner);
+
+	void removeSpawner(@NotNull Location location);
+
+	void createSpawner(@NonNull final Spawner spawner, final BiConsumer<Boolean, Spawner> consumer);
+
+	void deleteSpawner(@NonNull final Spawner spawner, final Consumer<Boolean> success);
+
+	Spawner findSpawner(@NotNull Location location);
+
+	List<Spawner> getSpawners();
+
+	int getSpawnerCountWithinChunk(@NonNull final Chunk chunk);
+
+	boolean canPlaceSpawnerInChunk(@NonNull final Chunk chunk);
+
+	void changeSpawnerEntity(@NonNull final CreatureSpawner spawner, @NonNull final EntityType entityType);
+
+	void applySpawnerDefaults(@NonNull final CreatureSpawner spawner, final boolean update);
+
+	void applySpawnerLevel(@NonNull final CreatureSpawner spawner, @NonNull final Level level);
+
+	void addPreset(@NonNull Preset preset);
+
+	void removePreset(@NonNull String key);
+
+	void createPreset(@NonNull final Preset preset, final BiConsumer<Boolean, Preset> consumer);
+
+	void deletePreset(@NonNull final Preset preset, final Consumer<Boolean> success);
+
+	Preset findPreset(@NonNull String key);
+
+	List<Preset> getPresets();
+
+	void addUser(@NonNull SpawnerUser spawnerUser);
+
+	void removeUser(@NonNull UUID userUUID);
+
+	void createUser(@NonNull final Player player, final BiConsumer<Boolean, SpawnerUser> consumer);
+
+	SpawnerUser findUser(@NonNull UUID userUUID);
+
+	SpawnerUser findUser(@NonNull final Player player);
+
+	List<SpawnerUser> getUsers();
+
 }
--- src/main/java/ca/tweetzy/spawners/impl/APIImplementation.java
+++ src/main/java/ca/tweetzy/spawners/impl/APIImplementation.java
@@ -17,7 +17,25 @@
  */
 package ca.tweetzy.spawners.impl;
 
+import ca.tweetzy.spawners.Spawners;
+import ca.tweetzy.spawners.api.LevelOption;
 import ca.tweetzy.spawners.api.SpawnersAPI;
+import ca.tweetzy.spawners.api.spawner.Level;
+import ca.tweetzy.spawners.api.spawner.Preset;
+import ca.tweetzy.spawners.api.spawner.Spawner;
+import ca.tweetzy.spawners.api.spawner.SpawnerUser;
+import lombok.NonNull;
+import org.bukkit.Chunk;
+import org.bukkit.Location;
+import org.bukkit.block.CreatureSpawner;
+import org.bukkit.entity.EntityType;
+import org.bukkit.entity.Player;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.List;
+import java.util.UUID;
+import java.util.function.BiConsumer;
+import java.util.function.Consumer;
 
 /**
  * Date Created: May 10 2022
@@ -26,4 +44,159 @@ import ca.tweetzy.spawners.api.SpawnersAPI;
  * @author Kiran Hart
  */
 public final class APIImplementation implements SpawnersAPI {
+
+	@Override
+	public void addLevel(@NonNull Level level) {
+		Spawners.getLevelManager().add(level);
+	}
+
+	@Override
+	public void removeLevel(@NonNull Level level) {
+		Spawners.getLevelManager().remove(level);
+	}
+
+	@Override
+	public void createLevel(@NonNull Level level, BiConsumer<Boolean, Level> consumer) {
+		Spawners.getLevelManager().createLevel(level, consumer);
+	}
+
+	@Override
+	public void deleteLevel(@NonNull Level level, Consumer<Boolean> consumer) {
+		Spawners.getLevelManager().deleteLevel(level, consumer);
+	}
+
+	@Override
+	public Level findLevel(@NonNull LevelOption levelOption, int levelNumber) {
+		return Spawners.getLevelManager().find(levelOption, levelNumber);
+	}
+
+	@Override
+	public List<Level> getLevels() {
+		return Spawners.getLevelManager().getContents();
+	}
+
+	@Override
+	public List<Level> getLevels(@NonNull LevelOption levelOption) {
+		return Spawners.getLevelManager().getLevels(levelOption);
+	}
+
+	@Override
+	public int getHighestLevel(@NonNull LevelOption levelOption) {
+		return Spawners.getLevelManager().getHighestLevel(levelOption);
+	}
+
+	@Override
+	public void addSpawner(@NotNull Spawner spawner) {
+		Spawners.getSpawnerManager().add(spawner);
+	}
+
+	@Override
+	public void removeSpawner(@NotNull Location location) {
+		Spawners.getSpawnerManager().remove(location);
+	}
+
+	@Override
+	public void createSpawner(@NonNull Spawner spawner, BiConsumer<Boolean, Spawner> consumer) {
+		Spawners.getSpawnerManager().createSpawner(spawner, consumer);
+	}
+
+	@Override
+	public void deleteSpawner(@NonNull Spawner spawner, Consumer<Boolean> success) {
+		Spawners.getSpawnerManager().deleteSpawner(spawner, success);
+	}
+
+	@Override
+	public Spawner findSpawner(@NotNull Location location) {
+		return Spawners.getSpawnerManager().find(location);
+	}
+
+	@Override
+	public List<Spawner> getSpawners() {
+		return Spawners.getSpawnerManager().getContents();
+	}
+
+	@Override
+	public int getSpawnerCountWithinChunk(@NonNull Chunk chunk) {
+		return Spawners.getAPI().getSpawnerCountWithinChunk(chunk);
+	}
+
+	@Override
+	public boolean canPlaceSpawnerInChunk(@NonNull Chunk chunk) {
+		return Spawners.getAPI().canPlaceSpawnerInChunk(chunk);
+	}
+
+	@Override
+	public void changeSpawnerEntity(@NonNull CreatureSpawner spawner, @NonNull EntityType entityType) {
+		Spawners.getSpawnerManager().changeSpawnerEntity(spawner, entityType);
+	}
+
+	@Override
+	public void applySpawnerDefaults(@NonNull CreatureSpawner spawner, boolean update) {
+		Spawners.getSpawnerManager().applySpawnerDefaults(spawner, update);
+	}
+
+	@Override
+	public void applySpawnerLevel(@NonNull CreatureSpawner spawner, @NonNull Level level) {
+		Spawners.getSpawnerManager().applySpawnerLevel(spawner, level);
+	}
+
+	@Override
+	public void addPreset(@NonNull Preset preset) {
+		Spawners.getPresetManager().add(preset);
+	}
+
+	@Override
+	public void removePreset(@NonNull String key) {
+		Spawners.getPresetManager().remove(key);
+	}
+
+	@Override
+	public void createPreset(@NonNull Preset preset, BiConsumer<Boolean, Preset> consumer) {
+		Spawners.getPresetManager().createPreset(preset, consumer);
+	}
+
+	@Override
+	public void deletePreset(@NonNull Preset preset, Consumer<Boolean> success) {
+		Spawners.getPresetManager().deletePreset(preset, success);
+	}
+
+	@Override
+	public Preset findPreset(@NonNull String key) {
+		return Spawners.getPresetManager().find(key);
+	}
+
+	@Override
+	public List<Preset> getPresets() {
+		return Spawners.getPresetManager().getContents();
+	}
+
+	@Override
+	public void addUser(@NonNull SpawnerUser spawnerUser) {
+		Spawners.getPlayerManager().add(spawnerUser);
+	}
+
+	@Override
+	public void removeUser(@NonNull UUID userUUID) {
+		Spawners.getPlayerManager().remove(userUUID);
+	}
+
+	@Override
+	public void createUser(@NonNull Player player, BiConsumer<Boolean, SpawnerUser> consumer) {
+		Spawners.getPlayerManager().createPlayer(player, consumer);
+	}
+
+	@Override
+	public SpawnerUser findUser(@NonNull UUID userUUID) {
+		return Spawners.getPlayerManager().find(userUUID);
+	}
+
+	@Override
+	public SpawnerUser findUser(@NonNull Player player) {
+		return Spawners.getPlayerManager().findUser(player);
+	}
+
+	@Override
+	public List<SpawnerUser> getUsers() {
+		return Spawners.getPlayerManager().getContents();
+	}
 }
